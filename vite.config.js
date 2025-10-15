f1ef800de5656f1a5be8f804c8aa8066ac797c47
Mykhailo Fysiuk <mishayo53@gmail.com>
2025-10-15 10:47:25 +0200

    initial config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/vite-react-project-template/",
+  base: "/",
   plugins: [react()],
 });
