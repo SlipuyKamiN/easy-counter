dfdb89ed2d5339a5fc84f54211bbb4d2ea1c0f61
Mykhailo Fysiuk <mishayo53@gmail.com>
2025-10-15 10:55:14 +0200

    build: basic set up

---
+++ src/styles/common/vars.js
@@ -0,0 +1,43 @@
+export const colors = {
+  light: {
+    hi200: "#F2F2F7",
+    hi100: "#C7C7CC",
+    mid100: "#8E8E93",
+    lo100: "#48484A",
+    lo200: "#1C1C1E",
+    gradient:
+      "linear-gradient(90deg,rgba(142, 142, 147, 0.4) 0%, rgba(28, 28, 30, 0.8) 60%, rgba(72, 72, 74, 0.6) 100%);",
+    logo: {
+      initCap:
+        "linear-gradient(90deg,rgba(255, 255, 255, 1) 0%,rgba(0, 0, 0, 1) 600%);",
+      main: "linear-gradient(90deg,rgba(255, 255, 255, 1) 0%,rgba(0, 0, 0, 1) 100%);",
+    },
+  },
+  dark: {
+    hi200: "#1C1C1E",
+    hi100: "#48484A",
+    mid100: "#8E8E93",
+    lo100: "#C7C7CC",
+    lo200: "#F2F2F7",
+    gradient:
+      "linear-gradient(90deg,rgba(242, 242, 247, 0.4) 0%, rgba(199, 199, 204, 0.5) 60%, rgba(242, 242, 247, 0.6) 100%)",
+    logo: {
+      initCap:
+        "linear-gradient(90deg,rgba(0, 0, 0, 1) 0%,rgba(255, 255, 255, 1) 600%);",
+      main: "linear-gradient(90deg,rgba(0, 0, 0, 1) 0%,rgba(255, 255, 255, 1) 100%);",
+    },
+  },
+  accent: {
+    cyan: "#64D2FF",
+    blue: "#0A84FF",
+    darkBlue: "#0040DD",
+  },
+  classicBlack: "#000000",
+  classicWhite: "#ffffff",
+  errorRed: "#FF0000",
+  success: "limegreen",
+};
+
+export const transition = {
+  duration: "250ms cubic-bezier(0.4, 0, 0.2, 1);",
+};
