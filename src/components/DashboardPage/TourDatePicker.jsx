cecb6af06c249323fbf5d3dd15d947109ea212ba
Mykhailo Fysiuk <mishayo53@gmail.com>
2025-11-27 18:09:04 +0100

    fix: date picker logic

--- src/components/DashboardPage/TourDatePicker.jsx
+++ src/components/DashboardPage/TourDatePicker.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import DatePicker from "react-multi-date-picker";
 
 export const TourDatePicker = ({ item, handleChange }) => {
-  const [dates, setDates] = useState(item.nextCheckout);
+  const [dates, setDates] = useState(item.nextCheckout || []);
+  const ref = useRef(item.nextCheckout || []);
 
   return (
     <DatePicker
@@ -13,11 +14,15 @@ export const TourDatePicker = ({ item, handleChange }) => {
       multiple
       minDate={new Date().setHours(0, 0, 0, 0)}
       value={dates}
-      onChange={setDates}
+      onChange={(v) => {
+        const arr = Array.isArray(v) ? v : [];
+        ref.current = arr;
+        setDates(arr);
+      }}
       onClose={() =>
         handleChange(item.id, {
           ...item,
-          nextCheckout: dates,
+          nextCheckout: Array.isArray(ref.current) ? ref.current : [],
         })
       }
       sort
