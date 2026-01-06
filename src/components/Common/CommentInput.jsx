d27ecfd26ccbfb5a5eb71d4c7068ca18d4ff6dc3
Mykhailo Fysiuk <mishayo53@gmail.com>
2026-01-06 18:06:03 +0100

    build: clear comment input

--- src/components/Common/CommentInput.jsx
+++ src/components/Common/CommentInput.jsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
 import { TextInput } from "./Inputs.styled";
+import { RxCross2 } from "react-icons/rx";
+
+import {
+  ClearInputButton,
+  TextInputWrapper,
+} from "../CounterPage/CounterPage.styled";
 
 export const CommentInput = ({ item, handleChange }) => {
   const [value, setValue] = useState(item.comment || "");
@@ -13,12 +19,28 @@ export const CommentInput = ({ item, handleChange }) => {
     });
   };
   return (
-    <TextInput
-      name="comment"
-      type="text"
-      value={value}
-      onChange={({ target }) => setValue(target.value)}
-      onBlur={onBlur}
-    />
+    <TextInputWrapper>
+      <TextInput
+        name="comment"
+        type="text"
+        value={value}
+        onChange={({ target }) => setValue(target.value)}
+        onBlur={onBlur}
+      />
+      {value && (
+        <ClearInputButton
+          type="button"
+          onClick={() => {
+            setValue("");
+            handleChange(item.id, {
+              ...item,
+              comment: "",
+            });
+          }}
+        >
+          <RxCross2 size={24} />
+        </ClearInputButton>
+      )}
+    </TextInputWrapper>
   );
 };
--- src/components/CounterPage/CounterPage.styled.jsx
+++ src/components/CounterPage/CounterPage.styled.jsx
@@ -67,42 +67,63 @@ export const CounterItem = styled.li`
     display: flex;
     justify-content: center;
     gap: 15px;
-
-    input {
-      font-size: 30px;
-      width: 90px;
-      text-align: center;
-      border: none;
-      padding: 0;
-
-      background-color: transparent;
-      border-bottom: 1px solid ${colors.light.hi100};
-    }
-
-    button {
-      width: 40px;
-      height: 40px;
-      padding: 0;
-
-      color: ${colors.light.lo200};
-      background-color: transparent;
-      border-radius: 50%;
-      font-size: 36px;
-      font-weight: 600;
-      line-height: 0;
-
-      border: 1px solid ${colors.light.hi100};
-      transition: ${transition.duration};
-
-      &:hover,
-      &:focus {
-        color: ${colors.light.hi200};
-        background-color: ${colors.light.mid100};
-      }
-
-      &:disabled {
-        color: ${colors.light.hi200};
-      }
-    }
   }
+`;
+
+export const CounterInput = styled.input`
+  font-size: 30px;
+  width: 90px;
+  text-align: center;
+  border: none;
+  padding: 0;
+
+  background-color: transparent;
+  border-bottom: 1px solid ${colors.light.hi100};
+`;
+
+export const CounterButton = styled.button`
+  width: 40px;
+  height: 40px;
+  padding: 0;
+
+  color: ${colors.light.lo200};
+  background-color: transparent;
+  border-radius: 50%;
+  font-size: 36px;
+  font-weight: 600;
+  line-height: 0;
+
+  border: 1px solid ${colors.light.hi100};
+  transition: ${transition.duration};
+
+  &:hover,
+  &:focus {
+    color: ${colors.light.hi200};
+    background-color: ${colors.light.mid100};
+  }
+
+  &:disabled {
+    color: ${colors.light.hi200};
+  }
+`;
+
+export const TextInputWrapper = styled.div`
+  position: relative;
+
+  max-width: 240px;
+  margin: 0 auto;
+
+  overflow: hidden;
+`;
+
+export const ClearInputButton = styled(CounterButton)`
+  position: absolute;
+  right: 1px;
+  top: 1px;
+
+  height: 32px;
+  width: 32px;
+
+  border: none;
+  background-color: ${colors.classicWhite};
 `;
